c707e49107cd4d67b835b736985be8e72c3abb9d
pyxudev <xsq51py@gmail.com>
2025-04-18 00:15:32 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client";
 export default function Home() {
+  function search() {
+    if (typeof window !== 'undefined') {
+      const keyword = document.getElementById("keyword") as HTMLInputElement;
+      const api = 'NEXT_PUBLIC_API_URL';
+      fetch(api,
+        {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          mode: 'cors',
+          body: JSON.stringify({ kw: keyword.value })
+        })
+        .then(response => response.json())
+        .then(data => {
+          const result = document.getElementById("results") as HTMLDivElement;
+          result.innerHTML = "";
+          for (let i = 1; i < Object.keys(data).length; i++) {
+            const div = document.createElement("div");
+            div.className = "game";
+            div.innerHTML = `<a href='${data[i].link}' target='_blank'><h3>チーム名：${data[i].teamname}</h3><p>参加条件：${data[i].requirements}</p><p>開催時間：${data[i].time}</p><p>場所：${data[i].address}</p></a>`;
+            result.appendChild(div);
+          }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    // <>
+    //   <header className="site-header">
+    //     <div className="wrapper site-header__wrapper">
+    //       <div className="site-header__logo">バスケやろうぜ</div>
+    //     </div>
+    //   </header>
+      <main>
+        <div className="search-form">
+          <h1>東京・千葉・埼玉検索</h1>
+          <div className="search-items">
+            <input type="text" id="keyword" />
+            <button id="search-btn" onClick={() => search()}>Search</button>
+          </div>
         </div>
+
+        <div id="results"></div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    // </>
   );
 }
